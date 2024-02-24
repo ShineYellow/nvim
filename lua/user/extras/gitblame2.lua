d4c14108127e19a02ef1531272f75236473ade71
Sammy Huang <sammy.huang@zilliz.com>
2024-02-24 21:06:28 +0800

    add blame
    Signed-off-by: Sammy Huang <sammy.huang@zilliz.com>

---
+++ lua/user/extras/gitblame2.lua
@@ -0,0 +1,13 @@
+local M = {
+  "FabijanZulj/blame.nvim"
+
+}
+
+M.config = function()
+
+  -- local wk = require "which-key"
+  -- wk.register {
+  --   ["<leader>gt"] = { "<cmd>ToggleBlame<cr>", "Toggle Blame" },
+  -- }
+end
+return M
