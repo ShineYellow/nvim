3ea0a12c3dba46a72c2c23eea3fc5c669e3e8152
Sammy Huang <sammy.huang@zilliz.com>
2024-02-24 20:08:10 +0800

    add shortcut for diff view
    Signed-off-by: Sammy Huang <sammy.huang@zilliz.com>

--- lua/user/diffview.lua
+++ lua/user/diffview.lua
@@ -5,5 +5,14 @@ local M = {
 }
 
 function M.config() end
+  require("dap-go").setup()
+
+  local wk = require "which-key"
+  wk.register {
+    ["<leader>gdh"] = { "<cmd>DiffviewFileHistory %<cr>", "current file history" },
+    ["<leader>gdH"] = { "<cmd>DiffviewFileHistory<cr>", "current branch history" },
+    ["<leader>gdx"] = { "<cmd>DiffviewClose<cr>", "close" },
+    ["<leader>gdt"] = { "<cmd>DiffviewToggleFiles<cr>", "Toggle the file panel" },
+  }
 
 return M
