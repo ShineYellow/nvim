5c395cc854e94cdcf404650c7f8281dcb145fc8b
Liang Huang <sammy.huang@zilliz.com>
2024-03-04 13:39:49 +0800

    add code actions
    Signed-off-by: Liang Huang <sammy.huang@zilliz.com>

--- lua/user/lspconfig.lua
+++ lua/user/lspconfig.lua
@@ -23,6 +23,7 @@ local function lsp_keymaps(bufnr)
   keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
   keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
   keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+  keymap(bufnr, "n", "ga", "<cmd>lua vim.lsp.buf.code_action({apply=true}) <CR>", opts)
 end
 
 M.on_attach = function(client, bufnr)
