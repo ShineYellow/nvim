eaffaac03693284676a2469798037638aea622a6
Liang Huang <sammy.huang@zilliz.com>
2024-05-13 10:00:08 +0800

    update shortcut form test
    Signed-off-by: Liang Huang <sammy.huang@zilliz.com>

--- lua/user/extras/neotest.lua
+++ lua/user/extras/neotest.lua
@@ -24,12 +24,19 @@ function M.config()
   local wk = require "which-key"
   wk.register {
     ["<leader>tt"] = { "<cmd>lua require'neotest'.run.run()<cr>", "Test Nearest" },
+    -- ["<leader>tT"] = { "<cmd>lua require'neotest'.run.run(vim.fn.getcwd())<cr>", "Test whole current directory" },
+    -- ["<leader>tW"] = { "<cmd>lua require'neotest'.watch.toggle(vim.fn.expand('%'))<cr>", "Toggle watch file" },
+    -- ["<leader>tw"] = { "<cmd>lua require'neotest'.watch.toggle()<cr>", "Toggle watch nearest" },
+    ["<leader>tl"] = { "<cmd>lua require'neotest'.run.run_last()<cr>", "Test Last" },
     ["<leader>tf"] = { "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>", "Test File" },
     ["<leader>td"] = { "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>", "Debug Test" },
-    ["<leader>ts"] = { "<cmd>lua require('neotest').run.stop()<cr>", "Test Stop" },
-    ["<leader>ta"] = { "<cmd>lua require('neotest').run.attach()<cr>", "Attach Test" },
-    ["<leader>to"] = { "<cmd>lua require('neotest').output.open()<cr>", "Test outut preview" },
-    ["<leader>tp"] = { "<cmd>lua require('neotest').output_panel.toggle()<cr>", "Test output panel" },
+    -- ["<leader>tS"] = { "<cmd>lua require('neotest').run.stop()<cr>", "Test Stop" },
+    -- ["<leader>ta"] = { "<cmd>lua require('neotest').run.attach()<cr>", "Attach Test" },
+    ["<leader>to"] = { "<cmd>lua require('neotest').output.open()<cr>", "outut preview" },
+    ["<leader>ts"] = { "<cmd>lua require('neotest').summary.toggle()<cr>", "Toggle summary" },
+    -- ["<leader>tq"] = { "<cmd>lua require('neotest').quickfix()<cr>", "Test quickfix" },
+    ["<leader>tp"] = { "<cmd>lua require('neotest').output_panel.toggle()<cr>", "Toggle panel" },
+    ["<leader>tc"] = { "<cmd>lua require('neotest').output_panel.clear()<cr>", "Clear panel" },
     ["<leader>tj"] = { "<cmd>lua require('neotest').jump.next({ status = 'failed' })<CR>", "Next Failed" },
     ["<leader>tk"] = { "<cmd>lua require('neotest').jump.prev({ status = 'failed' })<CR>", "Prev Failed" },
   }
