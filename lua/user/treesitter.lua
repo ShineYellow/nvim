6b3db9267a86e44d12a7176ad21ae256de6fb10b
huangliang1203 <huangliang1203@gmail.com>
2024-08-11 23:35:38 +0800

    add treesitter for vue

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -17,7 +17,18 @@ function M.config()
   }
 
   require("nvim-treesitter.configs").setup {
-    ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python" }, -- put the language you want in this array
+    ensure_installed = {
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "bash",
+      "python",
+      "javascript",
+      "typescript",
+      "html",
+      "css",
+      " scss ",
+    }, -- put the language you want in this array
     ignore_install = { "" },
     sync_install = false,
     highlight = {
