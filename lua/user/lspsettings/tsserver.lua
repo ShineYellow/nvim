cee1f68c30bb7ae4e246953be89f0d2fdb781a2c
huangliang1203 <huangliang1203@gmail.com>
2024-08-11 22:41:13 +0800

    enable global typescript for volar

--- lua/user/lspsettings/tsserver.lua
+++ lua/user/lspsettings/tsserver.lua
@@ -1,6 +1,13 @@
 local mason_registry = require('mason-registry')
 local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server'
 
+--from https://github.com/vuejs/language-tools?tab=readme-ov-file#community-integration
+-- The "Take Over" mode has been discontinued. 
+-- Instead, a new "Hybrid" mode has been introduced. 
+-- In this mode, the Vue Language Server exclusively manages the CSS/HTML sections. 
+-- As a result, you must run @vue/language-server in conjunction with a TypeScript server that employs @vue/typescript-plugin.
+-- Below is a streamlined configuration for Neovim's LSP, updated to accommodate the language server following the upgrade to version 2.0.0.
+
 return {
   init_options = {
     plugins = {
--- lua/user/lspsettings/volar.lua
+++ lua/user/lspsettings/volar.lua
@@ -1,2 +1,10 @@
 return {
+   init_options = {
+    typescript = {
+      tsdk = '/Users/admin/.asdf/installs/nodejs/20.14.0/lib/node_modules/typescript/lib'
+      -- Alternative location if installed as root:
+      -- tsdk = '/usr/local/lib/node_modules/typescript/lib'
+    }
+  },
+  filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'}
 }
