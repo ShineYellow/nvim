2d5028b4bd7c2f44f911e8ac6d85150d14177a5a
Yellow Shine <sammy.huang@zilliz.com>
2024-09-27 10:02:26 +0800

    update avante
    Signed-off-by: Yellow Shine <sammy.huang@zilliz.com>

--- lua/user/extras/avante.lua
+++ lua/user/extras/avante.lua
@@ -43,60 +43,77 @@ local M = {
 
 function M.config()
   require("avante").setup {
-    ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
-    provider = "openai", -- Recommend using Claude
-    auto_suggestions_provider = "copilot", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
-    openai = {
-      endpoint = "https://api.deepbricks.ai/v1/",
-      model = "GPT-4o",
-      temperature = 0,
-      max_tokens = 4096,
-    },
-    mappings = {
-      --- @class AvanteConflictMappings
-      diff = {
-        ours = "co",
-        theirs = "ct",
-        all_theirs = "ca",
-        both = "cb",
-        cursor = "cc",
-        next = "]x",
-        prev = "[x",
-      },
-      jump = {
-        next = "]]",
-        prev = "[[",
-      },
-      submit = {
-        normal = "<CR>",
-        insert = "<C-s>",
-      },
-    },
-    hints = { enabled = true },
-    windows = {
-      ---@type "right" | "left" | "top" | "bottom"
-      position = "right", -- the position of the sidebar
-      wrap = true, -- similar to vim.o.wrap
-      width = 30, -- default % based on available width
-      sidebar_header = {
-        align = "center", -- left, center, right for title
-        rounded = true,
-      },
-    },
-    highlights = {
-      ---@type AvanteConflictHighlights
-      diff = {
-        current = "DiffText",
-        incoming = "DiffAdd",
-      },
-    },
-    --- @class AvanteConflictUserConfig
+  ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
+  provider = "openai",
+  auto_suggestions_provider = "copilot",
+  openai = {
+    endpoint = "https://api.deepbricks.ai/v1/",
+    model = "GPT-4o",
+    temperature = 0,
+    max_tokens = 4096,
+  },
+  behaviour = {
+    auto_suggestions = true, -- Experimental stage
+    auto_set_highlight_group = true,
+    auto_set_keymaps = true,
+    auto_apply_diff_after_generation = false,
+    support_paste_from_clipboard = false,
+  },
+  mappings = {
+    --- @class AvanteConflictMappings
     diff = {
-      autojump = true,
-      ---@type string | fun(): any
-      list_opener = "copen",
+      ours = "co",
+      theirs = "ct",
+      all_theirs = "ca",
+      both = "cb",
+      cursor = "cc",
+      next = "]x",
+      prev = "[x",
     },
-  }
+    suggestion = {
+      accept = "<M-l>",
+      next = "<M-]>",
+      prev = "<M-[>",
+      dismiss = "<C-]>",
+    },
+    jump = {
+      next = "]]",
+      prev = "[[",
+    },
+    submit = {
+      normal = "<CR>",
+      insert = "<C-s>",
+    },
+    sidebar = {
+      switch_windows = "<Tab>",
+      reverse_switch_windows = "<S-Tab>",
+    },
+  },
+  hints = { enabled = true },
+  windows = {
+    ---@type "right" | "left" | "top" | "bottom"
+    position = "right", -- the position of the sidebar
+    wrap = true, -- similar to vim.o.wrap
+    width = 30, -- default % based on available width
+    sidebar_header = {
+      align = "center", -- left, center, right for title
+      rounded = true,
+    },
+  },
+  highlights = {
+    ---@type AvanteConflictHighlights
+    diff = {
+      current = "DiffText",
+      incoming = "DiffAdd",
+    },
+  },
+  --- @class AvanteConflictUserConfig
+  diff = {
+    autojump = true,
+    ---@type string | fun(): any
+    list_opener = "copen",
+  },
+}
 end
 
 return M
