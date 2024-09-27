21f86ed99dfd55659d04175c31b5afd8850bba07
Yellow Shine <sammy.huang@zilliz.com>
2024-09-27 10:17:56 +0800

    update volar
    Signed-off-by: Yellow Shine <sammy.huang@zilliz.com>

--- lua/user/lspsettings/volar.lua
+++ lua/user/lspsettings/volar.lua
@@ -1,7 +1,7 @@
 return {
    init_options = {
     typescript = {
-      tsdk = '/Users/admin/.asdf/installs/nodejs/20.14.0/lib/node_modules/typescript/lib'
+      tsdk = os.getenv("HOME") .. '/.asdf/installs/nodejs/20.14.0/lib/node_modules/typescript/lib'
       -- Alternative location if installed as root:
       -- tsdk = '/usr/local/lib/node_modules/typescript/lib'
     }
