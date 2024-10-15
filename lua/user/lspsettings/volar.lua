4a9e160c41a3b3db495dad58676e05ed96055f0c
Yellow Shine <sammy.huang@zilliz.com>
2024-10-15 11:42:37 +0800

    update vue language
    Signed-off-by: Yellow Shine <sammy.huang@zilliz.com>

--- lua/user/lspsettings/volar.lua
+++ lua/user/lspsettings/volar.lua
@@ -1,7 +1,10 @@
 return {
    init_options = {
     typescript = {
-      tsdk = os.getenv("HOME") .. '/.asdf/installs/nodejs/20.14.0/lib/node_modules/typescript/lib'
+      -- tsdk = os.getenv("HOME") .. '/.asdf/installs/nodejs/20.14.0/lib/node_modules/typescript/lib'
+      -- pnpm add -g typescript
+      -- pnpm root -g
+      tsdk = os.getenv("HOME") .. '/Library/pnpm/global/5/node_modules/typescript/lib'
       -- Alternative location if installed as root:
       -- tsdk = '/usr/local/lib/node_modules/typescript/lib'
     }
