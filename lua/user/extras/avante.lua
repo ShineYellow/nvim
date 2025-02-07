36d800e269f70dbf0ab420d51f868f8cb4b50134
Yellow Shine <sammy.huang@zilliz.com>
2025-02-07 19:22:38 +0800

    add deep seek
    Signed-off-by: Yellow Shine <sammy.huang@zilliz.com>

--- lua/user/extras/avante.lua
+++ lua/user/extras/avante.lua
@@ -39,101 +39,111 @@ local M = {
       opts = {
         file_types = { "markdown", "Avante" },
       },
-      ft = { "markdown", "Avante" }
+      ft = { "markdown", "Avante" },
     },
   },
 }
 
 function M.config()
-  require('copilot').setup ({
+  require("copilot").setup {
     -- use recommended settings from above
-  })
+  }
   require("avante").setup {
-  ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
-  -- provider = "openai",
-  provider = "copilot",
-  auto_suggestions_provider = "copilot",
-  openai = {
-    endpoint = "https://api.deepbricks.ai/v1/",
-    model = "Claude-3.5-Sonnet",
-    temperature = 0,
-    max_tokens = 4096,
-  },
-  copilot = {
-    endpoint = "https://api.githubcopilot.com",
+    ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
+    -- provider = "openai",
+    provider = "copilot",
+    -- provider = "deepseek",
+    auto_suggestions_provider = "copilot",
+    openai = {
+      endpoint = "https://api.deepbricks.ai/v1/",
+      model = "Claude-3.5-Sonnet",
+      temperature = 0,
+      max_tokens = 4096,
+    },
+    copilot = {
+      endpoint = "https://api.githubcopilot.com",
 
-    -- https://github.com/zed-industries/zed/blob/main/crates/copilot/src/copilot_chat.rs
-    model = "claude-3.5-sonnet",
-    -- model = "o1-preview-2024-09-12",
-    -- model = "o1-mini-2024-09-12",
-    proxy = nil, -- [protocol://]host[:port] Use this proxy
-    allow_insecure = false, -- Allow insecure server connections
-    timeout = 30000, -- Timeout in milliseconds
-    temperature = 0,
-    max_tokens = 200000,
-  },
-  behaviour = {
-    auto_suggestions = true, -- Experimental stage
-    auto_set_highlight_group = true,
-    auto_set_keymaps = true,
-    auto_apply_diff_after_generation = false,
-    support_paste_from_clipboard = false,
-  },
-  mappings = {
-    --- @class AvanteConflictMappings
+      -- https://github.com/zed-industries/zed/blob/main/crates/copilot/src/copilot_chat.rs
+      model = "claude-3.5-sonnet",
+      -- model = "o1-preview-2024-09-12",
+      -- model = "o1-mini-2024-09-12",
+      proxy = nil, -- [protocol://]host[:port] Use this proxy
+      allow_insecure = false, -- Allow insecure server connections
+      timeout = 30000, -- Timeout in milliseconds
+      temperature = 0,
+      max_tokens = 200000,
+    },
+
+    vendors = {
+      deepseek = {
+        __inherited_from = "openai",
+        api_key_name = "DEEPSEEK_API_KEY",
+        endpoint = "https://api.deepseek.com",
+        model = "deepseek-chat",
+      },
+    },
+    behaviour = {
+      auto_suggestions = true, -- Experimental stage
+      auto_set_highlight_group = true,
+      auto_set_keymaps = true,
+      auto_apply_diff_after_generation = false,
+      support_paste_from_clipboard = false,
+    },
+    mappings = {
+      --- @class AvanteConflictMappings
+      diff = {
+        ours = "co",
+        theirs = "ct",
+        all_theirs = "ca",
+        both = "cb",
+        cursor = "cc",
+        next = "]x",
+        prev = "[x",
+      },
+      suggestion = {
+        accept = "<C-l>",
+        next = "<C-n>",
+        prev = "<C-p>",
+        dismiss = "<C-]>",
+      },
+      jump = {
+        next = "]]",
+        prev = "[[",
+      },
+      submit = {
+        normal = "<CR>",
+        insert = "<C-s>",
+      },
+      sidebar = {
+        switch_windows = "<Tab>",
+        reverse_switch_windows = "<S-Tab>",
+     },
+    },
+    hints = { enabled = true },
+    windows = {
+      ---@type "right" | "left" | "top" | "bottom"
+      position = "right", -- the position of the sidebar
+      wrap = true, -- similar to vim.o.wrap
+      width = 30, -- default % based on available width
+      sidebar_header = {
+        align = "center", -- left, center, right for title
+        rounded = true,
+      },
+    },
+    highlights = {
+      ---@type AvanteConflictHighlights
+      diff = {
+        current = "DiffText",
+        incoming = "DiffAdd",
+      },
+    },
+    --- @class AvanteConflictUserConfig
     diff = {
-      ours = "co",
-      theirs = "ct",
-      all_theirs = "ca",
-      both = "cb",
-      cursor = "cc",
-      next = "]x",
-      prev = "[x",
+      autojump = true,
+      ---@type string | fun(): any
+      list_opener = "copen",
     },
-    suggestion = {
-      accept = "<C-l>",
-      next = "<C-n>",
-      prev = "<C-p>",
-      dismiss = "<C-]>",
-    },
-    jump = {
-      next = "]]",
-      prev = "[[",
-    },
-    submit = {
-      normal = "<CR>",
-      insert = "<C-s>",
-    },
-    sidebar = {
-      switch_windows = "<Tab>",
-      reverse_switch_windows = "<S-Tab>",
-    },
-  },
-  hints = { enabled = true },
-  windows = {
-    ---@type "right" | "left" | "top" | "bottom"
-    position = "right", -- the position of the sidebar
-    wrap = true, -- similar to vim.o.wrap
-    width = 30, -- default % based on available width
-    sidebar_header = {
-      align = "center", -- left, center, right for title
-      rounded = true,
-    },
-  },
-  highlights = {
-    ---@type AvanteConflictHighlights
-    diff = {
-      current = "DiffText",
-      incoming = "DiffAdd",
-    },
-  },
-  --- @class AvanteConflictUserConfig
-  diff = {
-    autojump = true,
-    ---@type string | fun(): any
-    list_opener = "copen",
-  },
-}
+  }
 end
 
 return M
